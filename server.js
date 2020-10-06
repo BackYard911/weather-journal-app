38d967a74d9ecdafd9d1be16133ba43b1f92337d
BackYard911 <yousseftarek9801@gmail.com>
2020-10-06 18:16:29 +0200

    added functionality

--- server.js
+++ server.js
@@ -2,18 +2,40 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
+const bodyParser = require('body-parser');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+const { response } = require('express');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8800;
+const server = app.listen(port,listening);
+
+app.get('/data', (req,res)=>{
+    res.send(projectData);
+    console.log("get + "+projectData);
+})
+
+app.post('/data',(req,res) =>{
+    console.log(projectData);
+    projectData = req.body;
+    console.log(req.body);
+    res.send("success");
+})
+
+function listening(){
+    console.log('server is running');
+    console.log(`check localhost/${port}`);
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,92 @@
-/* Global Variables */
 
+
+/* Global Variables */
+const apiKey = '&appid=92ad8be999a1c3fbb3df2efff1f03966&units=metric';
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+
+const generateBtn = document.querySelector('#generate');
+const zipBox = document.querySelector('#zip');
+const feelingBox = document.querySelector('#feelings');
+
+const date = document.querySelector('#date');
+const temp = document.querySelector('#temp');
+const content = document.querySelector('#content');
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+generateBtn.addEventListener('click',buttonPressed);
+
+function buttonPressed(){
+    let zip = zipBox.value;
+    fetchData(baseUrl,zip,apiKey)
+    .then(data => {
+    const postedData = {
+        date : newDate,
+        feels_like : data.main.temp + "°C",
+        feelings : feelingBox.value
+        
+    };
+    
+    postData('/data',postedData)
+    .then(()=>{
+        getData('/data')
+        .then((allData) =>{
+            updateUI(allData);
+            
+        })
+    })
+    
+})
+}
+
+const fetchData = async (baseUrl,zip,apiKey)=>{
+    const res = await fetch(baseUrl+zip+apiKey)
+    try{
+        const data = await res.json();
+        console.log(data);
+        return data;
+    }
+    catch(error){
+        console.log("error",error);
+    }
+}
+
+const getData = async (localUrl) =>{
+    const res = await fetch(localUrl)
+    
+    try{
+        const data = await res.json();
+        console.log(data);
+        return data;
+    }
+    catch(error){
+        console.log("error",error);
+    }
+}
+
+const postData = async (localUrl ='' , data = {}) => {
+    console.log(localUrl);
+    const res = await fetch(localUrl, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {'content-type': 'application/json',},
+        body: JSON.stringify(data),
+    });
+    console.log("success");
+    try{
+        const newData = await res.text();
+        console.log(newData);
+        return newData;
+    }
+    catch(error){
+        console.log("error",error);
+    }
+}
+
+function updateUI(data){
+    date.innerHTML = data.date;
+    temp.innerHTML = data.feels_like;
+    content.innerHTML = data.feelings || "do u feel nothing? :(";
+}
+
